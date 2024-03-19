dadaa574b9b46c5f5f31da256fabb0b69d221761
Addaal <adalmontania@gmail.com>
2024-03-19 11:17:48 -0300

    inicizlizacion de proyecto

---
+++ src/main/java/com/roshka/thbackend/ThbackendApplication.java
@@ -0,0 +1,13 @@
+package com.roshka.thbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roshka/thbackend/ThbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshka.thbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
