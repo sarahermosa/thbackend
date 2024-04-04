2e9c6a106dcf304d2b3984d68348a2d904dc2ea2
Addaal <adalmontania@gmail.com>
2024-04-04 09:43:15 -0400

    email beneficios

--- src/main/java/com/roshka/thbackend/utils/ScheduledTask.java
+++ src/main/java/com/roshka/thbackend/utils/ScheduledTask.java
@@ -1,13 +1,19 @@
 package com.roshka.thbackend.utils;
 
 import com.roshka.thbackend.service.IBeneficio;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Component
@@ -22,14 +28,40 @@ public class ScheduledTask {
     private JavaMailSender javaMailSender;
     //@Scheduled(cron = "0 0 12 1/15 * ?") //MANDA EL CORREO CADA 15 DIAS
     @Scheduled(fixedRate = 40000) // 10 seg
-    public void imprimirHolaMundo() {
+    public void imprimirHolaMundo() throws MessagingException, IOException {
         List<String> nombresBeneficios = beneficioService.obtenerNombresBeneficios();
 
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo("ferledesma352@gmail.com");
+        String ListHTML = "";
+        int count = 1;
+        for (String beneficio : nombresBeneficios)
+        {
+            String ptag = "<p style=\"font-size: 1rem; color:black;\">" + count + ". " + beneficio + "</p>";
+            ListHTML += ptag;
+            count++;
+        }
+
+//        SimpleMailMessage email = new SimpleMailMessage();
+//        email.setTo("ferledesma352@gmail.com");
+//        email.setFrom("bootcampjava341@gmail.com");
+//        email.setSubject("Beneficios");
+//        email.setText("Lista De Beneficios:\n" + String.join("\n*", nombresBeneficios)+"\nNO RESPONDER A ESTE MENSAJE");
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper email = new MimeMessageHelper(message, true);
+
+        email.setTo("adalmontania@gmail.com");
         email.setFrom("bootcampjava341@gmail.com");
         email.setSubject("Beneficios");
-        email.setText("Lista De Beneficios:\n" + String.join("\n*", nombresBeneficios)+"\nNO RESPONDER A ESTE MENSAJE");
-        javaMailSender.send(email);
+
+        String htmlFilePath = "src/main/resources/templates/schedule-task-template.html";
+        String logoUrl = "https://i.postimg.cc/rpYs8GHX/roshka-logo-white.png"; // Replace with the actual URL of your image
+
+        String htmlContent = new String(Files.readAllBytes(Paths.get(htmlFilePath)));
+        htmlContent = htmlContent.replace("{lista}", ListHTML);
+
+        htmlContent = htmlContent.replace("{logo}", logoUrl);
+
+        email.setText(htmlContent, true);
+        javaMailSender.send(message);
     }
 }
